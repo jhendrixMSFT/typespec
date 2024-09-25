ea4819ac3c29f9f2f74edc89833ac59eadcbcdbc
Joel Hendrix <jhendrix@microsoft.com>
2024-09-25 10:05:33 -0700

    test indentation

--- packages/http-client-csharp/src/components/models-file.tsx
+++ packages/http-client-csharp/src/components/models-file.tsx
@@ -1,4 +1,4 @@
-import { mapJoin } from "@alloy-js/core";
+import { IndentContext, mapJoin } from "@alloy-js/core";
 import * as csharp from "@alloy-js/csharp";
 import { Model } from "@typespec/compiler";
 import * as ef from "@typespec/emitter-framework/csharp";
@@ -11,6 +11,7 @@ export interface ModelsFileProps {
 export function ModelsFile(props: ModelsFileProps) {
   return (
     <csharp.SourceFile path={props.path ?? "models.cs"}>
+      <IndentContext.Provider value={{ indent: "    ", indentString: "", level: 0 }}>
       {mapJoin(
         props.models,
         (model) => {
@@ -18,6 +19,7 @@ export function ModelsFile(props: ModelsFileProps) {
         },
         { joiner: "\n\n" }
       )}
+      </IndentContext.Provider>
     </csharp.SourceFile>
   );
 }
